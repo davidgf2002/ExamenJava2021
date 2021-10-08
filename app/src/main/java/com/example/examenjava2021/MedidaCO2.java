7c84c574e747164a6dcd157effde9745dd7521de
David <deividgf567@gmail.com>
2021-10-08 15:49:32 +0200

    Clase "MedidaCO2"

---
+++ app/src/main/java/com/example/examenjava2021/MedidaCO2.java
@@ -0,0 +1,53 @@
+package com.example.examenjava2021;
+
+public class MedidaCO2 {
+
+    //Declaración de los atributos
+    private int tiempo;
+    private int concentracionElectronica;
+    private int concentracionTelematica;
+    private int concentracionAula9;
+
+
+    //Métodos Get y Set para acceder a los atributos
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public int getConcentracionElectronica() {
+        return concentracionElectronica;
+    }
+
+    public void setConcentracionElectronica(int concentracionElectronica) {
+        this.concentracionElectronica = concentracionElectronica;
+    }
+
+    public int getConcentracionTelematica() {
+        return concentracionTelematica;
+    }
+
+    public void setConcentracionTelematica(int concentracionTelematica) {
+        this.concentracionTelematica = concentracionTelematica;
+    }
+
+    public int getConcentracionAula9() {
+        return concentracionAula9;
+    }
+
+    public void setConcentracionAula9(int concentracionAula9) {
+        this.concentracionAula9 = concentracionAula9;
+    }
+
+
+    //Constructor
+    public MedidaCO2(int tiempo, int concentracionElectronica, int concentracionTelematica, int concentracionAula9) {
+        this.tiempo = tiempo;
+        this.concentracionElectronica = concentracionElectronica;
+        this.concentracionTelematica = concentracionTelematica;
+        this.concentracionAula9 = concentracionAula9;
+    }
+}
